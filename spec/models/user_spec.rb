6a62b8badd7b2766d19aef99a9bcaad625ba9851
rayves <vo.raymond.v@outlook.com>
2022-08-14 18:23:22 +1000

    wraps group User model tests | creates testing file for Store model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,56 +2,58 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validation tests' do
-    it 'ensures email is present' do
-      user = User.new(username: 'Ray').save
-      expect(user).to eq(false)
-    end
+    context 'registration' do
+      it 'ensures email is present' do
+        user = User.new(username: 'Ray').save
+        expect(user).to eq(false)
+      end
 
-    it 'ensures email is valid' do
-      user = User.new(email: 'test@example', username: 'Ray').save
-      expect(user).to eq(false)
-    end
+      it 'ensures email is valid' do
+        user = User.new(email: 'test@example', username: 'Ray').save
+        expect(user).to eq(false)
+      end
 
-    it 'ensures username is present' do
-      user = User.new(email: 'test@example').save
-      expect(user).to eq(false)
-    end
+      it 'ensures username is present' do
+        user = User.new(email: 'test@example').save
+        expect(user).to eq(false)
+      end
 
-    it 'ensures username is unique' do
-      User.create(email: 'test@example.com', username: 'Ray', password: 'password')
-      user = User.new(email: 'test2@example.com', username: 'Ray', password: 'password').save
-      expect(user).to eq(false)
-    end
+      it 'ensures username is unique' do
+        User.create(email: 'test@example.com', username: 'Ray', password: 'password')
+        user = User.new(email: 'test2@example.com', username: 'Ray', password: 'password').save
+        expect(user).to eq(false)
+      end
 
-    it 'ensures password is present' do
-      user = User.new(email: 'test@example.com', username: 'Ray').save
-      expect(user).to eq(false)
-    end
+      it 'ensures password is present' do
+        user = User.new(email: 'test@example.com', username: 'Ray').save
+        expect(user).to eq(false)
+      end
 
-    it 'ensures password is encrypted' do
-      user = User.create(email: 'test@example.com', username: 'Ray', password: 'password')
-      expect(user.password_digest).not_to eq('password')
-    end
+      it 'ensures password is encrypted' do
+        user = User.create(email: 'test@example.com', username: 'Ray', password: 'password')
+        expect(user.password_digest).not_to eq('password')
+      end
 
-    it 'should save successfully with valid data' do
-      user = User.new(email: 'test@example.com', username: 'Ray', password: 'password').save
-      expect(user).to eq(true)
-    end
+      it 'should save successfully with valid data' do
+        user = User.new(email: 'test@example.com', username: 'Ray', password: 'password').save
+        expect(user).to eq(true)
+      end
 
-    it 'ensures users are not admin by default' do
-      user = User.create(email: 'test@example.com', username: 'Ray', password: 'password')
-      expect(user.admin).to eq(false)
-    end
+      it 'ensures users are not admin by default' do
+        user = User.create(email: 'test@example.com', username: 'Ray', password: 'password')
+        expect(user.admin).to eq(false)
+      end
 
-    it 'should make registered user admin is assigned so' do
-      user = User.create(email: 'test@example.com', username: 'Ray', password: 'password', admin: true)
-      expect(user.admin).to eq(true)
-    end
+      it 'should make registered user admin is assigned so' do
+        user = User.create(email: 'test@example.com', username: 'Ray', password: 'password', admin: true)
+        expect(user.admin).to eq(true)
+      end
 
-    it 'should only register one user per email used' do
-      User.new(email: 'test@example.com', username: 'Ray', password: 'password').save
-      user_two = User.new(email: 'test@example.com', username: 'Ray2', password: 'password').save
-      expect(user_two).to eq(false)
+      it 'should only register one user per email used' do
+        User.new(email: 'test@example.com', username: 'Ray', password: 'password').save
+        user_two = User.new(email: 'test@example.com', username: 'Ray2', password: 'password').save
+        expect(user_two).to eq(false)
+      end
     end
   end
 end
